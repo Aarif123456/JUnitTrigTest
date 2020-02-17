e402c0c6e88063ecb9a982454c0aa67918056595
Abdullah Arif <arif115@uwindsor.ca>
2020-02-16 20:44:15 -0500

    'Created test cases - Version 1.0'

---
+++ src/Trignometry.java
@@ -0,0 +1,30 @@
+// Abdullah Arif
+// February 24, 2020
+// Program to calculate sin, cos and tan given in radians
+public class Trignometry {
+    // Function to calculate sin where input is in radians
+    public static double sin(double radian){
+        if(Double.isNaN(radian))
+            return Double.NaN;
+        return radian;
+    }
+
+    // Function to calculate cos where input is given in radians
+    public static double cos(double radian){
+        if(Double.isNaN(radian))
+            return Double.NaN;
+
+        return radian;
+    }
+
+    // Function to calculate tan where input is given in radians
+    public static double tan(double radian){
+        if(Double.isNaN(radian))
+            return Double.NaN;
+        return radian;
+    }
+
+    public static double toRadians(double degrees){
+        return degrees;
+    }
+}
